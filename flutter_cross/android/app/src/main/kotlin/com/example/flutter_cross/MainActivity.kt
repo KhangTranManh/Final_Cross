c6241bc7d5ae6b3e53491b150d16ceb60182bb7a
KhangTran <khangjaki12@gmail.com>
2025-08-27 07:38:24 +0700

    update flutter

---
+++ flutter_cross/android/app/src/main/kotlin/com/example/flutter_cross/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_cross
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ flutter_cross/lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const flutter_cross());
+} 
